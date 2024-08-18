fcb87bce2ec92cbe19cf12f3117d82fb833ae904
Shady Adel <shadyadel910@gmail.com>
2024-08-18 19:58:20 +0300

    intial design for main sports screen and leagues screen

---
+++ SportsApp/SportsApp/Modules/sports/SportsVC.swift
@@ -0,0 +1,109 @@
+//
+//  MainSportsCollectionVC.swift
+//  SportsApp
+//
+//  Created by Shady Adel on 14/08/2024.
+//
+
+import UIKit
+
+
+
+class SportsVC: UICollectionViewController,UICollectionViewDelegateFlowLayout {
+
+    let sportsArray = ["baseball", "basketball", "cricket", "soccer"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        self.tabBarController?.navigationItem.title = "Sports"
+       
+    }
+
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+       
+        return 1
+    }
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+       
+        return sportsArray.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if let leagueVC = self.storyboard?.instantiateViewController(withIdentifier: "leagueVc") as? LeaguesTableVC {
+            leagueVC.sport = sportsArray[indexPath.row]
+            self.navigationController?.pushViewController(leagueVC, animated: true)
+        }
+    }
+    
+    
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! SportsCell
+        
+        cell.imgMainSportImage.image = UIImage(named: sportsArray[indexPath.row])
+        cell.lblMainSportLabel.text = sportsArray[indexPath.row].capitalized
+       
+    
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let numberOfItemsPerRow: CGFloat = 2
+        let spacingBetweenCells: CGFloat = 10
+        
+        let totalSpacing = (2 * spacingBetweenCells) + ((numberOfItemsPerRow - 1) * spacingBetweenCells)
+        let width = (collectionView.bounds.width - totalSpacing) / numberOfItemsPerRow
+        
+        return CGSize(width: width, height: width)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 10
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 10
+    }
+   
+   
+
+    // MARK: UICollectionViewDelegate
+
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+    
+    }
+    */
+
+}
